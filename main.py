aa8aaa55f033a6424fe873de2557c2cc11b9eae5
lupto <luke.r.upton@gmail.com>
2017-05-25 16:05:12 -0400

    Added .csv conversion
    Script can now convert .csv files in the following format to .pcb:
    element, x, y, z
    Likewise, the script can do the reverse

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ generate_command_list = [["direct", "Direct atomic coordinate entry"],
 global modify_command_list
 modify_command_list = []
 global convert_command_list
-convert_command_list = []
+convert_command_list = [['atoms pdb -> csv','Copies element and coordinate info into csv'],
+['atoms csv -> pdb', 'Creates pdb document based on csv with elements and coordinates'],
+['exit', 'Return to starting menu']]
 global writefile    # FID for file that is written to in operations
 global tempfile		# FID for temporary file in modify operations
-global filename		# Filename input by user
+global filename		# PDB Filename input by user
 filename = ''
 global file_array
 file_array = []		# Array of all information in file
@@ -43,12 +45,13 @@ global general_command_list # Universal commands
 general_command_list = [["view", "View contents of file"],
 ["exit", "Save and return to starting menu"]
 ]
-
+global make_new_PDB # Conversion mode:  Signals when a new PDB is needed (csv -> pdb, etc.)
+make_new_PDB = 0
 
 ### Functions ###
 
 
-# GENERATE MODE
+# GENERATE/MODIFY MODE
 def write_atom(element, x, y, z):  # write an atom's coordinates to the next atom line in a file
 	global atom_end_line
 	global atom_start_line
@@ -106,7 +109,81 @@ def element_correction(element):
 		output = element   # This could lead to an error if somebody 
 							# puts in around a 3 character long element name
 	return output
+	
 
+# CONVERT MODE 
+def write_atoms_to_csv():  # Converts the atomic information in a .pdb to a csv format
+	global writefile
+	global file_array
+	global atom_start_line
+	global atom_end_line
+	global filename
+	output_line = '' # placeholder for output line
+	csv_target = '' # Placeholder for csv filename
+	filename = ''
+	
+	while (filename == ''):
+		filename = input("Please enter a PDB filename: ")
+		filename = filename.strip('.pdb')
+		if (os.path.isfile(filename + '.pdb')):
+			file_initialization() # Open and access contents of PDB file
+		else:
+			print('Error: cannot access file')
+			time.sleep(1)
+			filename = ''
+	csv_target = input("\nEnter filename of target .csv file: ")
+	csv_target = csv_target.strip('.csv')
+	print('Overwriting/creating {0}.csv'.format(csv_target))
+	csv_fid = open(csv_target + '.csv', 'w') #Overwrites existing files, creates otherwise
+	for i in range(atom_start_line, atom_end_line + 1):
+		output_line = '{0},{1},{2},{3}\n'.format(file_array[i][2], file_array[i][6], file_array[i][7],file_array[i][8])
+		csv_fid.write(output_line)
+	
+	csv_fid.close()
+	close_files()
+	
+def write_atoms_from_csv():
+	global make_new_PDB
+	global writefile
+	global file_array
+	global atom_start_line
+	global atom_end_line
+	global filename
+	make_new_PDB = 1 # we want a new document here
+	csv_target = ''
+	filename = ''
+	temp_array = [] # array for storing csv info
+	
+	while(csv_target == ''):
+		csv_target = input("\nEnter filename of input .csv file: ")
+		csv_target = csv_target.strip('.csv')
+		if (os.path.isfile(csv_target + '.csv')):
+			csv_fid = open(csv_target + '.csv', 'r')
+		else:
+			print("Error: cannot find specified file.")
+			csv_target = ''
+			time.sleep(1)
+	while(filename == ''):
+		filename = input("\nEnter filename of output .pdb file: ")
+		filename = filename.strip('.pdb')
+		if (not os.path.isfile(filename + '.pdb')):
+			file_initialization()
+		else:
+			print("Error: cannot overwrite file.")
+			filename = ''
+			time.sleep(1)
+	
+	# We can write atoms in now
+	temp_array = csv_fid.readlines()
+	
+	for i in range(0, len(temp_array)):
+		temp_array[i] = temp_array[i].strip().split(',')
+		write_atom(temp_array[i][0], temp_array[i][1], temp_array[i][2], temp_array[i][3])
+	
+	    
+	csv_fid.close()
+	close_files()
+		
 	
 	
 	
@@ -118,7 +195,7 @@ def file_initialization():  #Opens All necessary files
 	global filename
 	global file_array
 	filename = filename.strip('.pdb')
-	if (mode == 'generate'):
+	if (mode == 'generate') or ((mode == 'convert') and (make_new_PDB == 1)):
 		if (os.path.isfile(filename + '.pdb')):
 			print("Error, file exists. Cannot generate.")
 			time.sleep(2) # 2 second delay
@@ -136,7 +213,17 @@ def file_initialization():  #Opens All necessary files
 			writefile.write("SCALE1      0.010163  0.000000  0.006427        0.00000\n")
 			writefile.write("SCALE2      0.000000  0.015002  0.000000        0.00000\n")
 			writefile.write("SCALE3      0.000000  0.000000  0.015381        0.00000\n")
-	elif (mode != 'generate'):
+	elif (mode == 'modify'):
+		if(os.path.isfile(filename + '.pdb')):
+			writefile = open(filename + '.pdb', 'r+') #Opens output file for reading and writing
+			#if (mode == 'modify'):
+				#tempfile = open(filename + '_temp.pdb', 'w') #Establishes a clean temporary file
+		else:
+			print("Error, file does not exist.")
+			time.sleep(2)
+			mode_select()
+	elif ((mode == 'convert') and (make_new_PDB == 0)):
+		#print(filename)
 		if(os.path.isfile(filename + '.pdb')):
 			writefile = open(filename + '.pdb', 'r+') #Opens output file for reading and writing
 			#if (mode == 'modify'):
@@ -159,9 +246,10 @@ def close_files():  #Closes all opened files to prevent I/O Errors
 	global atom_end_line
 	atom_start_line = 0  # Reset atom block markers to original conditions
 	atom_end_line = 0
-	# write edits to file
-	rewrite()
-	writefile.close()
+	if (mode != 'debug'):
+		# write edits to file
+		rewrite()
+		writefile.close()
 
 def rewrite():  # Overwrites existing file
 	global file_array
@@ -194,7 +282,7 @@ def rewrite():  # Overwrites existing file
 				writefile.write('    ')
 		if(i != len(file_array)-1):
 			writefile.write('\n')
-	if (mode == 'generate'):
+	if (mode == 'generate') or ((mode == 'convert') and (make_new_PDB == 1)):
 		writefile.write('MASTER      336    0    1    9   10    0    1    6 2822    4    4   24   \n')
 		writefile.write('END')
 	
@@ -271,9 +359,13 @@ def check_command(command):  # Checks if a user command is correct.  If correct,
 		help()
 	if (command == 'exit'):
 		global mode
-		close_files()
+		global filename
+		if (mode != 'convert'):
+			close_files()
+		make_new_PDB = 0  # Prevents i/o error with convert mode
 		legitimate_command = 1
 		mode = ''
+		filename = ''
 		mode_select()
 	if (command == 'view'):
 		legitimate_command = 1
@@ -285,6 +377,13 @@ def check_command(command):  # Checks if a user command is correct.  If correct,
 		elif(command == 'helix'):
 			legitimate_command = 1
 			helix()
+	elif (mode == 'convert'):
+		if(command == 'atoms pdb -> csv'):
+			legitimate_command = 1
+			write_atoms_to_csv()
+		if(command == 'atoms csv -> pdb'):
+			legitimate_command = 1
+			write_atoms_from_csv()
 			
 def help():	# Help command for each mode
 	if (mode == 'generate'):
@@ -302,11 +401,12 @@ def help():	# Help command for each mode
 		print("\n\nMode specific commands:")
 	for i in range(0, len(helplist)):
 		print(helplist[i][0] + " - " + helplist[i][1])
-	if (debug == 1):
-		print("\n\nUniversal Commands:")
-	for i in range(0, len(general_command_list)):
-		print(general_command_list[i][0] + " - " + general_command_list[i][1])	
-	print("\n")
+	if (mode != 'convert'):
+		if (debug == 1):
+			print("\n\nUniversal Commands:")
+		for i in range(0, len(general_command_list)):
+			print(general_command_list[i][0] + " - " + general_command_list[i][1])	
+		print("\n")
 	
 def mode_select(): #User selects mode
 	global mode
@@ -332,8 +432,9 @@ def mode_select(): #User selects mode
 	# User has selected a mode
 	os.system('cls')  # Clear Screen
 	print("Welcome to " + mode + " mode.\n\n")
-	filename = input("Please enter a target filename: ")
-	file_initialization() # Open files for appropriate modifications
+	if (mode != 'convert'):
+		filename = input("Please enter a target filename: ")
+		file_initialization() # Open files for appropriate modifications
 	print("\nType 'help' to access a listing of commands.\n")
 	prompt_command()
 	
